977368824958ea173cf4573294480d971df0b664
Alireza Dehghan <44303528+alirezax0098@users.noreply.github.com>
2019-07-05 22:41:48 +0430

    Update WTMparser.py

--- WTMparser.py
+++ WTMparser.py
@@ -5,7 +5,26 @@ soup = BeautifulSoup(page.content,'html.parser')
 fulltable = soup.find("table", { "class" : "table table-sm table-hover table-vcenter" })
 rows = fulltable.findAll("tr")
 tableMatrix=[]
+NamePriceMatrix=[]
 for i in range(len(rows)):
     tableMatrix.append(rows[i].findAll("td"))
 tableMatrix.remove([])
-print(tableMatrix)
+
+#remove the unnecessary entries that come with the td tag
+j=0
+while j<len(tableMatrix):
+    if (tableMatrix[j][0].text.find('(') == -1):
+        tableMatrix.remove(tableMatrix[j])
+    else:
+        j=j+1
+
+#get the raw price and names and put them in a new matrix
+for i in range (len(tableMatrix)):
+    first = tableMatrix[i][7].text.find('$')+1
+    last = tableMatrix[i][7].text.find('$', first+1)
+    String = tableMatrix[i][7].text[first:last]
+    Price = float(String)
+    Name = tableMatrix[i][0].text.strip()
+    NamePriceMatrix.append([Name, Price])
+
+print(NamePriceMatrix)
